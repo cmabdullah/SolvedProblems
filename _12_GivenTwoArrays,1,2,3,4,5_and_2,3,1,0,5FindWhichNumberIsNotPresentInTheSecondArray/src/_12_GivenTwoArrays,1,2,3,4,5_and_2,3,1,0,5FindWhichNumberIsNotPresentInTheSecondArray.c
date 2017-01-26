9c88bf763552e80b1139eaa2d8ecefac5dd02063
C M Abdullah Khan <cmabdullah21@gmail.com>
2017-01-26 10:40:14 +0600

    start new project named _12_GivenTwoArrays,1,2,3,4,5_and_2,3,1,0,5FindWhichNumberIsNotPresentInTheSecondArray

---
+++ _12_GivenTwoArrays,1,2,3,4,5_and_2,3,1,0,5FindWhichNumberIsNotPresentInTheSecondArray/src/_12_GivenTwoArrays,1,2,3,4,5_and_2,3,1,0,5FindWhichNumberIsNotPresentInTheSecondArray.c
@@ -0,0 +1,23 @@
+/*
+ ============================================================================
+ Name        : _12_GivenTwoArrays,1,2,3,4,5_and_2,3,1,0,5FindWhichNumberIsNotPresentInTheSecondArray.c
+ Author      : AbdullahKhan
+ Version     : 0.01
+ Copyright   : OpenSource
+ Description : 12) Given two arrays, 1,2,3,4,5 and 2,3,1,0,5 find which number is not present in the second array.
+ Compiler    : Eclipse CDT ( C/C++ Development Tools )
+ Problem     : 12
+ Reference   : javarevisited.blogspot.com/2011/06/top-programming-interview-questions.html
+ Resource    :
+ Date        : 26-01-17
+ Path        :/Users/abdullah/Documents/SolvedProblems/_12_GivenTwoArrays,1,2,3,4,5_and_2,3,1,0,5FindWhichNumberIsNotPresentInTheSecondArray/src
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}

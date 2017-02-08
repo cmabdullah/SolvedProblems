71fe6a7ef8dc5bad2c1a550e978fa42ef075d7a6
CM Abdullah Khan <abdullah@CMs-MacBook-Air.local>
2017-02-09 01:21:02 +0600

    UVA3_11727 problem started

---
+++ UVA3_11727/src/UVA3_11727.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : UVA3_11727.c
+ Author      : AbdullahKhan
+ Version     :
+ Copyright   : OpenSource
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("UVA3_11727"); /* prints UVA3_11727 */
+	return EXIT_SUCCESS;
+}

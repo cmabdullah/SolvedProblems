b49133b55d5d7335e8b0ec65da55033cb90681f6
CM Abdullah Khan <abdullah@CMs-MacBook-Air.local>
2017-02-10 10:36:33 +0600

    I have completed UVA3_11727.c it another mathode

--- UVA3_11727/src/UVA3_11727.c
+++ UVA3_11727/src/UVA3_11727.c
@@ -10,28 +10,61 @@
 /***/
 #include <stdio.h>
 int main(void) {
-	int i , T , a , b,c;
+	int i , T , a , b,c, result;
 	scanf("%d", &T);
 	for (i = 1; i<=T; i++){
 		scanf("%d%d%d",&a,&b,&c);
-		if( a>=1000&&a<10000 && 	b>=1000&&b<=10000 && c>=1000&&c<=10000){
-					//Checking for a is middle number or not
+		if(a>=1000 && a<10000){
+		if(b>=1000 && b<10000) {
+		if(c>=1000 && c<10000) {
+
+			if( b>a && a>c || c>a && a>b ){
+					//	printf("Case %d :%d",i,a);
+						result = a;
+					}
+
+						        //Checking for b is middle number or not
+					if( a>b && b>c || c>b && b>a ){
+					//	printf("Case %d :%d",i,b);
+						result = b;
+					}
+
+						        //Checking for c is middle number or not
+					if( a>c && c>b || b>c && c>a ){
+						//printf("Case %d :%d",i,c);
+						result = c;
+					}
+					printf("Case %d :%d",i,result);
+
+
+				}
+			}
+		}
+
+
+
+			//printf("Case %d",i);
+			/***
 		if( b>a && a>c || c>a && a>b ){
-			printf("%d",a);
+		//	printf("Case %d :%d",i,a);
+			result = a;
 		}
 
 			        //Checking for b is middle number or not
 		if( a>b && b>c || c>b && b>a ){
-			printf("%d",b);
+		//	printf("Case %d :%d",i,b);
+			result = b;
 		}
 
 			        //Checking for c is middle number or not
 		if( a>c && c>b || b>c && c>a ){
-			printf("%d",c);
+			//printf("Case %d :%d",i,c);
+			result = c;
 		}
-	  }
-		else
-			printf("unsuccess");
-	}
+		printf("Case %d :%d",i,result);
+		***/
+
+}
 	return 0;
 }
+
--- test/src/test.c
+++ test/src/test.c
@@ -10,41 +10,31 @@
 //Following program printing all Armstrong number except 153.but //why
 // i can not fix it.needed help
 
-#include <stdio.h>
-int main(void) {
-    int n1, n2, n3;
-
-      int mid;
-      if( n1 > n2 )   {
-              if( n3 > n2)
-                    {
-                            if( n3 < n1 )
-                                    mid = n3;
-                            printf( "%d  ", mid );
-                            else
-                                    mid = n1;
-                            printf( "%d  ", mid );
-                    }
-             else {
-                    mid = n2;
-                    printf( "%d  ", mid );
-            }
-     }
-     else {
-            if( n2 > n3 )
-             {
-                    if ( n1 > n3)
-                      mid = n1;
-                    printf( "%d  ", mid );
-                    else
-                    mid = n3;
-                    printf( "%d  ", mid );
-             }
-            else{
-                    mid = n2;
-                    printf( "%d  ", mid );
-            }
+#include<stdio.h>
+int main()
+{
+    int t,i,a,b,c,ans;
+   while( scanf("%d",&t)==1){
+    for(i=1;i<=t;i++)
+    {
+    	scanf("%d %d %d",&a,&b,&c);
+    	if(a>1000||a<10000){
+    if(b>1000||b<10000){
+    if(c>1000||c<10000){
+    if((a>b && a<c)||(a<b&&a>c))
+        ans=a;
+    else if((b>a&&b<c)||(b<a&&b>c))
+        ans=b;
+    else if((c>b && c<a)||(c<b)&&c>a)
+        ans=c;
+    			}
+    		}
     }
-    return mid ;
+printf("Case %d: %d\n",i,ans);
+
+}
+return 0;
+
+}
 }
 

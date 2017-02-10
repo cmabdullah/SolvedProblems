3012edc4c72033e40102668959e4e8371b3009d4
CM Abdullah Khan <abdullah@CMs-MacBook-Air.local>
2017-02-10 20:38:28 +0600

    this problem is successfully accepted by uva(UVA3_11727.c)

--- UVA3_11727/src/UVA3_11727.c
+++ UVA3_11727/src/UVA3_11727.c
@@ -1,6 +1,6 @@
 /*
  ============================================================================
- Name        : UVA3_11727.c
+ Name        : UVA 11727.c
  Author      : AbdullahKhan
  Version     :
  Copyright   : OpenSource
@@ -10,61 +10,29 @@
 /***/
 #include <stdio.h>
 int main(void) {
-	int i , T , a , b,c, result;
-	scanf("%d", &T);
-	for (i = 1; i<=T; i++){
-		scanf("%d%d%d",&a,&b,&c);
-		if(a>=1000 && a<10000){
-		if(b>=1000 && b<10000) {
-		if(c>=1000 && c<10000) {
-
-			if( b>a && a>c || c>a && a>b ){
-					//	printf("Case %d :%d",i,a);
-						result = a;
+	int a,b,c,i,T,result;
+	scanf("%d",&T);
+		if (T<20){
+		for (i=1;i<=T;i++){
+			scanf("%d%d%d",&a,&b,&c);
+			if(a>=1000 && a<10000){
+				if(b>=1000 && b<10000){
+					if(c>=1000&&c<10000){
+						if((b>a&&a>c)||(c>a&&a>b)){
+						result=a;
+							}
+						if((a>b&&b>c)||(c>b&&b>a)){
+							result=b;
+						}
+						if((a>c&&c>b)||(b>c&&c>a)){
+							result = c;
+						}
 					}
-
-						        //Checking for b is middle number or not
-					if( a>b && b>c || c>b && b>a ){
-					//	printf("Case %d :%d",i,b);
-						result = b;
-					}
-
-						        //Checking for c is middle number or not
-					if( a>c && c>b || b>c && c>a ){
-						//printf("Case %d :%d",i,c);
-						result = c;
-					}
-					printf("Case %d :%d",i,result);
-
-
 				}
 			}
+			printf("Case %d: %d\n",i,result);
 		}
-
-
-
-			//printf("Case %d",i);
-			/***
-		if( b>a && a>c || c>a && a>b ){
-		//	printf("Case %d :%d",i,a);
-			result = a;
-		}
-
-			        //Checking for b is middle number or not
-		if( a>b && b>c || c>b && b>a ){
-		//	printf("Case %d :%d",i,b);
-			result = b;
-		}
-
-			        //Checking for c is middle number or not
-		if( a>c && c>b || b>c && c>a ){
-			//printf("Case %d :%d",i,c);
-			result = c;
-		}
-		printf("Case %d :%d",i,result);
-		***/
-
-}
+	}
 	return 0;
 }
 
--- test/src/test.c
+++ test/src/test.c
@@ -18,14 +18,14 @@ int main()
     for(i=1;i<=t;i++)
     {
     	scanf("%d %d %d",&a,&b,&c);
-    	if(a>1000||a<10000){
-    if(b>1000||b<10000){
-    if(c>1000||c<10000){
+    	if ( (a>1000) ||(a<10000 )){
+    if((b>1000)||(b<10000)){
+    if((c>1000)||(c<10000)){
     if((a>b && a<c)||(a<b&&a>c))
         ans=a;
-    else if((b>a&&b<c)||(b<a&&b>c))
+    else if(( (b>a) && (b<c))||((b<a) && (b>c)))
         ans=b;
-    else if((c>b && c<a)||(c<b)&&c>a)
+    else if(((c>b) && (c<a)) || ((c<b))&&(c>a)) 
         ans=c;
     			}
     		}

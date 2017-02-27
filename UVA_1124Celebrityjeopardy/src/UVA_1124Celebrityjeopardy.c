c4a2d8d6092ba88cc4611b80a8a6ee86cdba52a4
Abdullah Khan <cmabdullah21@gmail.com>
2017-02-28 01:57:58 +0600

    70%COMPLETE 1124

---
+++ UVA_1124Celebrityjeopardy/src/UVA_1124Celebrityjeopardy.c
@@ -0,0 +1,21 @@
+/*
+ ============================================================================
+ Name        : UVA_1124Celebrityjeopardy.c
+ Author      : AbdullahKhan
+ Version     :
+ Copyright   : OpenSource
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//#include <cstdio>
+int main(){
+    char s[12800];
+    while(gets(s)){
+    	printf("%s\n",s);
+    }
+    return 0;
+}

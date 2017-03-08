b04ec474cef638a1071ab0363c30bcd54ba0ab82
Abdullah Khan <cmabdullah21@gmail.com>
2017-03-08 22:13:21 +0600

    UVA11_11364Parking/src/UVA11_11635HotelBooking.c accepted by UVA

--- UVA11_11364Parking/src/UVA11_11635HotelBooking.c
+++ UVA11_11364Parking/src/UVA11_11635HotelBooking.c
@@ -9,35 +9,22 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
-
-int main(void) {
-
-	int arr[1000], i,j,temp, k = 0, sum = 0, n, T;
-	float  average, x,y, result;
-	scanf("%d", &T);
-	while(k<T){
-	scanf("%d", &n);
-	for (i = 0;i<n;i++){
-		scanf("%d",&arr[i]);
-		sum = sum + arr[i];
-	}
-	average = (float)sum/n;
-	//insertion sort
-	for(i=1;i<n;i++){
-		temp = arr[i];
-		j=i-1;
-		while(temp<arr[j] && j>=0){
-		arr[j+1] = arr[j];
-		--j;
+int main(void){
+	int val,min,max,n,T;
+	scanf("%d",&T);
+	while(T--){
+	scanf("%d",&n);
+	min=100;
+	max=0;
+	while(n--){
+		scanf("%d",&val);
+		if(val>max){
+			max = val;
 		}
-		arr[j+1]=temp;
+		if(val<min){
+			min=val;
 		}
-	x = average-	arr[0];
-	y = arr[n-1] - average;
-	result  = (x*2)+(y*2);
-	printf("%.0f",result);
-	k++;
 	}
-
+	printf("%d\n",2*(max-min));
+	}
 }

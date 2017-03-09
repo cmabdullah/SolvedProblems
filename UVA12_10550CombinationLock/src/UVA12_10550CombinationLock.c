788904bdce27f46d09c672ce763610f020c138ce
Abdullah Khan <cmabdullah21@gmail.com>
2017-03-09 23:15:48 +0600

    UVA12_10550CombinationLock started

---
+++ UVA12_10550CombinationLock/src/UVA12_10550CombinationLock.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : UVA12_10550CombinationLock.c
+ Author      : AbdullahKhan
+ Version     :
+ Copyright   : OpenSource
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("10550 Combination Lock"); /* prints 10550 Combination Lock */
+	return EXIT_SUCCESS;
+}

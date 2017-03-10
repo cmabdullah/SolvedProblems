29bc0091d95ddbea14fb95d6993307aeddbb5677
Abdullah Khan <cmabdullah21@gmail.com>
2017-03-10 23:37:34 +0600

    combination lock has been unlocked by me tonight :D

--- UVA12_10550CombinationLock/src/UVA12_10550CombinationLock.c
+++ UVA12_10550CombinationLock/src/UVA12_10550CombinationLock.c
@@ -12,6 +12,30 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("10550 Combination Lock"); /* prints 10550 Combination Lock */
-	return EXIT_SUCCESS;
+	int a,b,c,i, x,y,z;
+
+	while (scanf("%d %d %d %d",&i,&a,&b,&c)==4){
+//	scanf("%d%d%d%d",&i,&a,&b,&c);
+		 if (!i && !a && !b && !c)
+			 break;
+	if (a>i ){
+		x = (40-a)+i;
+	}
+	else{
+		x = i-a;
+	}
+	if (b<a ){
+			y = (40-a)+b;
+		}
+		else{
+			y = b-a;
+		}
+	if (c>b ){
+			z = (40-c)+b;
+		}
+		else{
+			z = b-c;
+		}
+	printf("%d\n",(360*3) + (x+y+z)*9 );
+	}
 }

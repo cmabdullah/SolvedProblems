c0f994c0bbab359e6f27f38cf79f6689b2bd13e3
Abdullah Khan <cmabdullah21@gmail.com>
2017-03-10 23:48:40 +0600

    just rearranged combination lock , nothing else

--- UVA12_10550CombinationLock/src/UVA12_10550CombinationLock.c
+++ UVA12_10550CombinationLock/src/UVA12_10550CombinationLock.c
@@ -9,33 +9,30 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
-
-int main(void) {
-	int a,b,c,i, x,y,z;
-
+int main(void){
+	int a,b,c,i,x,y,z;
 	while (scanf("%d %d %d %d",&i,&a,&b,&c)==4){
-//	scanf("%d%d%d%d",&i,&a,&b,&c);
-		 if (!i && !a && !b && !c)
-			 break;
-	if (a>i ){
-		x = (40-a)+i;
+		if (!i && !a && !b && !c)
+		break;
+	if(a>i){
+		x=(40-a)+i;
 	}
 	else{
-		x = i-a;
+		x=i-a;
 	}
-	if (b<a ){
-			y = (40-a)+b;
-		}
-		else{
-			y = b-a;
-		}
-	if (c>b ){
-			z = (40-c)+b;
-		}
-		else{
-			z = b-c;
-		}
-	printf("%d\n",(360*3) + (x+y+z)*9 );
+	if(b<a){
+		y=(40-a)+b;
 	}
+	else{
+		y=b-a;
+	}
+	if(c>b){
+		z=(40-c)+b;
+	}
+	else{
+		z=b-c;
+	}
+	printf("%d\n",(360*3)+(x+y+z)*9);
+	}
+	return 0;
 }

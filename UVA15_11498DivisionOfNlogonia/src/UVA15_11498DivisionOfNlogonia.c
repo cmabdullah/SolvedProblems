235c45da754420057a477d91ca58888216c6da09
Abdullah Khan <cmabdullah21@gmail.com>
2017-03-15 23:48:13 +0600

    problem  11498 Division Of Nlogonia started

---
+++ UVA15_11498DivisionOfNlogonia/src/UVA15_11498DivisionOfNlogonia.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : UVA15_11498DivisionOfNlogonia.c
+ Author      : AbdullahKhan
+ Version     :
+ Copyright   : OpenSource
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("11498DivisionOfNlogonia"); /* prints 11498DivisionOfNlogonia */
+	return EXIT_SUCCESS;
+}

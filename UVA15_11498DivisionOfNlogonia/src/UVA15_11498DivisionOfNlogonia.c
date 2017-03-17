8aa8cc8ba1606a1409003580a5f09dfe19cf0a1c
Abdullah Khan <cmabdullah21@gmail.com>
2017-03-17 17:33:33 +0600

    done UVA15_11498DivisionOfNlogonia.c

--- UVA15_11498DivisionOfNlogonia/src/UVA15_11498DivisionOfNlogonia.c
+++ UVA15_11498DivisionOfNlogonia/src/UVA15_11498DivisionOfNlogonia.c
@@ -9,9 +9,32 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
-
 int main(void) {
-	puts("11498DivisionOfNlogonia"); /* prints 11498DivisionOfNlogonia */
-	return EXIT_SUCCESS;
+	int K,N,M ,X,Y;
+	while(scanf("%d",&K)==1){
+		if (K==0)
+			break;
+		scanf("%d %d",&N,&M);
+		for(int i=0;i<K;i++){
+			scanf("%d %d",&X,&Y);
+			if(N==X || M ==Y){
+				printf("divisa\n");
+			}
+			else if(N<X&&M<Y){
+				printf("NE\n");
+			}
+			else if(N>X&&M<Y){
+				printf("NO\n");
+			}
+			else if(N<X&&M>Y){
+				printf("SE\n");
+			}
+			else if (X<N&&Y<M){
+				printf("SO\n");
+			}
+			else
+				printf("Wrong\n");
+		}
+	}
+	return 0;
 }

e9aac9cf2a66194f4a8bb1db37bf9d24b3308e1c
Abdullah Khan <cmabdullah21@gmail.com>
2017-03-18 12:56:05 +0600

    start new program UVA18_12289OneTwoThree

---
+++ UVA18_12289OneTwoThree/src/UVA18_12289OneTwoThree.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : UVA18_12289OneTwoThree.c
+ Author      : AbdullahKhan
+ Version     :
+ Copyright   : OpenSource
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("UVA18_12289OneTwoThree"); /* prints UVA18_12289OneTwoThree */
+	return EXIT_SUCCESS;
+}

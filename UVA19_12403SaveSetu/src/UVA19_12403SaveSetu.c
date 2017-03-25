a3f873bd90576ce8f00428fb36659e29335add35
Abdullah Khan <cmabdullah21@gmail.com>
2017-03-25 10:40:24 +0600

    Done UVA19_12403SaveSetu accepted by uva

--- UVA19_12403SaveSetu/src/UVA19_12403SaveSetu.c
+++ UVA19_12403SaveSetu/src/UVA19_12403SaveSetu.c
@@ -14,18 +14,17 @@ int main(void){
 	int K, T,amount= 0;
 	scanf("%d",&T);
 	while(T--){
-	char string_A[9] = "donate";
-	char string_B[9];
-	int x;
+	char string_A[100] = "donate";
+	char string_B[100];
 	scanf("%s", string_B);
-	x = strcmp(string_A,string_B);
-	if (0 == x){
+	if (strcmp(string_B,string_A) == 0){
 	scanf("%d", &K);
-	amount = amount+K;
+	amount=amount+K;
 	}
 	else
-	printf("report %d\n",amount);
+	printf("%d\n",amount);
 	}
+	return 0;
 }
 
 

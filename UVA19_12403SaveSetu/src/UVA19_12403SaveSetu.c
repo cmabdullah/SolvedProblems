b40a59b9d8e830dc7af60c3e91703db36644ba74
Abdullah Khan <cmabdullah21@gmail.com>
2017-03-25 10:39:54 +0600

    complete 99%

--- UVA19_12403SaveSetu/src/UVA19_12403SaveSetu.c
+++ UVA19_12403SaveSetu/src/UVA19_12403SaveSetu.c
@@ -9,14 +9,23 @@
  */
 
 #include <stdio.h>
+#include<string.h>
 int main(void){
 	int K, T,amount= 0;
-	scanf("%d", &T);
+	scanf("%d",&T);
 	while(T--){
-		printf("donate ");
+	char string_A[9] = "donate";
+	char string_B[9];
+	int x;
+	scanf("%s", string_B);
+	x = strcmp(string_A,string_B);
+	if (0 == x){
 	scanf("%d", &K);
 	amount = amount+K;
+	}
+	else
 	printf("report %d\n",amount);
-	T--;
 	}
 }
+
+

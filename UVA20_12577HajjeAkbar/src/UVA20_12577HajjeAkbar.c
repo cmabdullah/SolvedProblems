79c6dabc8fa0e4329f359400e4fee17615c26e88
Abdullah Khan <cmabdullah21@gmail.com>
2017-03-25 10:44:17 +0600

    Sttarted new problem UVA20_12577HajjeAkbar

---
+++ UVA20_12577HajjeAkbar/src/UVA20_12577HajjeAkbar.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : UVA20_12577HajjeAkbar.c
+ Author      : AbdullahKhan
+ Version     :
+ Copyright   : OpenSource
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("UVA20_12577HajjeAkbar"); /* prints UVA20_12577HajjeAkbar */
+	return EXIT_SUCCESS;
+}

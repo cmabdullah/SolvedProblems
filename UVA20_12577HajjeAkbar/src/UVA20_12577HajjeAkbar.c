9de934ad35e3a23bedfbf6ce0a91c0c55b65844f
Abdullah Khan <cmabdullah21@gmail.com>
2017-03-26 23:10:59 +0600

    UVA20_12577HajjeAkbar

--- UVA20_12577HajjeAkbar/src/UVA20_12577HajjeAkbar.c
+++ UVA20_12577HajjeAkbar/src/UVA20_12577HajjeAkbar.c
@@ -7,26 +7,22 @@
  Description : Hello World in C, Ansi-style
  ============================================================================
  */
-
 #include <stdio.h>
 #include <stdlib.h>
-
 int main(void) {
 	int K, i,amount= 0;
-
-		for (i = 1;i!= 0;i++){
-			if (i == 10)
-				break;
-
-		char H[100] = "Hajj";
-		char U[100];
+	char H[100] = "Hajj";
+	char U[100];
+	for (i = 1;i!=0;i++){
 		scanf("%s", U);
-		if (strcmp(U,H) != 0){
-			printf("Case %d: Hajj-e-Asghar\n",i);
+		if (U[0] == '*')
+		break;
+		if(strcmp(U,H) != 0){
+		printf("Case %d: Hajj-e-Asghar\n",i);
 		}
 		else{
 		printf("Case %d: Hajj-e-Akbar\n",i);
 		}
-		}
-		return 0;
+	}
+	return 0;
 }
--- test/src/test.c
+++ test/src/test.c
@@ -12,6 +12,29 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("test"); /* prints test */
-	return EXIT_SUCCESS;
+	int K, i,amount= 0;
+		char H[100] = "Hajj";
+		char U[100];
+
+		for (i = 1;i!= 0;i++){
+
+		scanf("%s", U);
+
+
+
+		 if (U[0] == '*')
+			 break;
+		if (strcmp(U,H) != 0){
+			printf("Case %d: Hajj-e-Asghar\n",i);
+		}
+
+		if (strcmp(U,H) == 0){
+					printf("Case %d: Hajj-e-Akbar\n",i);
+				}
+
+		else{
+		printf("null\n");
+		}
+		}
+		return 0;
 }

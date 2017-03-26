d1af49e3c28290fbfdbba68ba477178f03ac589a
Abdullah Khan <cmabdullah21@gmail.com>
2017-03-26 23:30:04 +0600

    HajjeAkbar accepted by uva, started new program 12250LanguageDetection

--- UVA21_12250LanguageDetection/src/UVA21_12250LanguageDetection.c
+++ UVA21_12250LanguageDetection/src/UVA21_12250LanguageDetection.c
@@ -1,28 +1,17 @@
 /*
  ============================================================================
- Name        : UVA20_12577HajjeAkbar.c
+ Name        : UVA21_12250LanguageDetection.c
  Author      : AbdullahKhan
  Version     :
  Copyright   : OpenSource
  Description : Hello World in C, Ansi-style
  ============================================================================
  */
+
 #include <stdio.h>
 #include <stdlib.h>
+
 int main(void) {
-	int  i;
-	char H[100] = "Hajj";
-	char U[100];
-	for (i = 1;i!=0;i++){
-		scanf("%s", U);
-		if (U[0] == '*')
-		break;
-		if(strcmp(U,H) != 0){
-		printf("Case %d: Hajj-e-Asghar\n",i);
-		}
-		else{
-		printf("Case %d: Hajj-e-Akbar\n",i);
-		}
-	}
-	return 0;
+	puts("12250LanguageDetection"); /* prints 12250LanguageDetection */
+	return EXIT_SUCCESS;
 }

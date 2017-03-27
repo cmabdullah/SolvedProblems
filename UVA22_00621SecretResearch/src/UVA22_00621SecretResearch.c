49808f0b2668e10089a444a832687902252366a8
Abdullah Khan <cmabdullah21@gmail.com>
2017-03-27 21:36:59 +0600

    UVA22_00621SecretResearch accepted by uva

--- UVA22_00621SecretResearch/src/UVA22_00621SecretResearch.c
+++ UVA22_00621SecretResearch/src/UVA22_00621SecretResearch.c
@@ -11,8 +11,29 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 int main(void) {
-	puts("UVA22_00621SecretResearch"); /* prints UVA22_00621SecretResearch */
-	return EXIT_SUCCESS;
+	int l, T;
+	char ar[1000];
+	scanf("%d", &T);
+	while (T--){
+		scanf("%s", ar);
+		l = strlen(ar);
+		if (ar[l - 1] == '5' && ar[l - 2] == '3'){
+		printf("-\n");
+		}
+		else if (strcmp(ar, "1") == 0 || strcmp(ar, "4") == 0|| strcmp(ar, "74")== 0){
+		printf("+\n");
+		}
+		else 	if (ar[0] == '1' && ar[1] == '9'&& ar[2] == '0'){
+		printf("?\n");
+		}
+		else if (ar[0] == '9' && ar[l-1] == '4'){
+		printf("*\n");
+		}
+		else{
+		printf("+\n");
+		}
+	}
+	return 0;
 }

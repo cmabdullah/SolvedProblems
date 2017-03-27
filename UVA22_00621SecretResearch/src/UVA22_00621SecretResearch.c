0d9d6e64b20de7aef5c675a6593031c86c2ddb4d
Abdullah Khan <cmabdullah21@gmail.com>
2017-03-27 19:52:47 +0600

    Start new program UVA22_00621SecretResearch

---
+++ UVA22_00621SecretResearch/src/UVA22_00621SecretResearch.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : UVA22_00621SecretResearch.c
+ Author      : AbdullahKhan
+ Version     :
+ Copyright   : OpenSource
+ Description : Hello World in C, Ansi-style
+ Reference	 : Competitive Programming 3, page 20.
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("UVA22_00621SecretResearch"); /* prints UVA22_00621SecretResearch */
+	return EXIT_SUCCESS;
+}

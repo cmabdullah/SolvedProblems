b45b42b5abe7f3b60b632d72de2b97acffae1ced
Abdullah Khan <cmabdullah21@gmail.com>
2017-03-27 19:47:27 +0600

    remodified UVA21_12250LanguageDetection and accepted by uva, a book Competitive Programming 3 is added and start new program '00621' : Secret Research

--- UVA21_12250LanguageDetection/src/UVA21_12250LanguageDetection.c
+++ UVA21_12250LanguageDetection/src/UVA21_12250LanguageDetection.c
@@ -13,35 +13,37 @@
 #include <stdlib.h>
 int main(void){
 	int  i;
-	char English[100]="HELLO";
-	char Spanish[100]="HOLA";
-	char German[100]="HALLO";
-	char French[100]="BONJOUR";
-	char Italian[100]="CIAO";
-	char Russian[100]="ZDRAVSTVUJTE";
+	//char English[100]="HELLO";
+//	char Spanish[100]="HOLA";
+//	char German[100]="HALLO";
+//	char French[100]="BONJOUR";
+//	char Italian[100]="CIAO";
+//	char Russian[100]="ZDRAVSTVUJTE";
 	char U[100];
 	for(i=1;i!=0;i++){
 		scanf("%s",U);
 		if(U[0]=='#')
 		break;
-		if(strcmp(U,English)==0){
+		else if(strcmp(U,"HELLO")==0){
 		printf("Case %d: ENGLISH\n",i);
 		}
-		if(strcmp(U,Spanish)==0){
+		else if(strcmp(U,"HOLA")==0){
 		printf("Case %d: SPANISH\n",i);
 		}
-		if(strcmp(U,German)==0){
+		else if(strcmp(U,"HALLO")==0){
 		printf("Case %d: GERMAN\n",i);
 		}
-		if(strcmp(U,French)==0){
+		else if(strcmp(U,"BONJOUR")==0){
 		printf("Case %d: FRENCH\n",i);
 		}
-		if(strcmp(U,Italian)==0){
+		else if(strcmp(U,"CIAO")==0){
 		printf("Case %d: ITALIAN\n",i);
 		}
-		if(strcmp(U,Russian)==0){
+		else if(strcmp(U,"ZDRAVSTVUJTE")==0){
 		printf("Case %d: RUSSIAN\n",i);
 		}
+		else
+		printf("Case %d: UNKNOWN\n",i);
 	}
 	return 0;
 }

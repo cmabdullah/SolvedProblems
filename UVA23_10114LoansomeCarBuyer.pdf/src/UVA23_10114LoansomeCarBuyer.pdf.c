9aae5e7d358f088be7bd0475302e93216f616744
Abdullah Khan <cmabdullah21@gmail.com>
2017-03-29 22:20:42 +0600

    start new program 10114LoansomeCarBuyer

---
+++ UVA23_10114LoansomeCarBuyer.pdf/src/UVA23_10114LoansomeCarBuyer.pdf.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : pdf.c
+ Author      : AbdullahKhan
+ Version     :
+ Copyright   : OpenSource
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("10114LoansomeCarBuyer.pdf"); /* prints 10114LoansomeCarBuyer.pdf */
+	return EXIT_SUCCESS;
+}

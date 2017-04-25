0a3ba5e6f04bd20851bcd8db11201d85b7356ead
Abdullah Khan <cmabdullah21@gmail.com>
2017-04-25 20:51:35 +0600

    10300EcologicalPremium started

---
+++ UVA24_10300_EcologicalPremium/src/UVA24_10300_EcologicalPremium.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : UVA24_10300_EcologicalPremium.c
+ Author      : AbdullahKhan
+ Version     :
+ Copyright   : OpenSource
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("10300 - Ecological Premium"); /* prints 10300 - Ecological Premium */
+	return EXIT_SUCCESS;
+}

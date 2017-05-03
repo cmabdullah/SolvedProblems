f04d86e3ff637769a6a3bf8a5c3aed48eaad7a83
Abdullah Khan <cmabdullah21@gmail.com>
2017-05-04 00:52:01 +0600

    Started 11679 Sub Prime

---
+++ Uva27_11679SubPrime/src/Uva27_11679SubPrime.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Uva27_11679SubPrime.c
+ Author      : AbdullahKhan
+ Version     :
+ Copyright   : OpenSource
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Uva27_11679SubPrime"); /* prints Uva27_11679SubPrime */
+	return EXIT_SUCCESS;
+}

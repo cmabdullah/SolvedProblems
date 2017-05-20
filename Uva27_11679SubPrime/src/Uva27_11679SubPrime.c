c13c5adb4734e4fb01ce312b5c68f0112f7f712a
Abdullah Khan <cmabdullah21@gmail.com>
2017-05-21 00:47:40 +0600

    changed file

--- Uva27_11679SubPrime/src/Uva27_11679SubPrime.c
+++ Uva27_11679SubPrime/src/Uva27_11679SubPrime.c
@@ -8,27 +8,4 @@
  ============================================================================
  */
 
-#include <stdio.h>
-int main(void){
-	int arr[100];
-	int i, j = 1, high, lo , n, T;
-scanf("%d", &T);
-while(T--){
-	scanf("%d",&n);
-	for (i = 0; i<n; i++){
-		scanf("%d",&arr[i]);
-	}
-	high = 0, lo = 0;
-	for (i = 0; i< n-1; i++){
-		//printf("%d ", arr[i]);
-		if (arr[i]< arr[i+1]){
-			high++;
-		}
-		//printf("loop %d arr = %d  arr+1 = %d high = %d \n",i,arr[i],arr[i+1], high);
-		if (arr[i]>arr[i+1])
-			lo++;
-	}
-	printf("Case %d: %d %d\n", j, high, lo);
-	j++;
-	}
-}
+//underconstrution

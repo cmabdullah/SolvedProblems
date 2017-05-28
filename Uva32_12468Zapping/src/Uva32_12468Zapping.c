1849bba2cc17a0115e2b1a488c918202e937da7b
Abdullah Khan <cmabdullah21@gmail.com>
2017-05-28 23:16:24 +0600

    Lunch new project Zapping

---
+++ Uva32_12468Zapping/src/Uva32_12468Zapping.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Uva32_12468Zapping.c
+ Author      : AbdullahKhan
+ Version     :
+ Copyright   : OpenSource
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("12468Zapping"); /* prints 12468Zapping */
+	return EXIT_SUCCESS;
+}

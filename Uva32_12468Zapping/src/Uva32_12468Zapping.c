a33aad870308f6857dde5e3ac454c297b71cdcf1
Abdullah Khan <cmabdullah21@gmail.com>
2017-05-30 23:47:44 +0600

    app done zapping

--- Uva32_12468Zapping/src/Uva32_12468Zapping.c
+++ Uva32_12468Zapping/src/Uva32_12468Zapping.c
@@ -7,11 +7,44 @@
  Description : Hello World in C, Ansi-style
  ============================================================================
  */
-
+/**
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void) {
-	puts("12468Zapping"); /* prints 12468Zapping */
-	return EXIT_SUCCESS;
+
+	int a,b,c;
+	scanf("%d%d",&a,&b);
+
+	if (a<b){
+		c = b-a;
+	printf("%d", c);
+	}
+	//a= 80
+	//b= 10
+	//99-80 +10
+	else{
+		c = 100-a+b;
+		printf("%d", c);
+	}
+
+}
+**/
+
+#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+    int T=200,a,b;
+    while (T--)
+    {
+        scanf("%d%d",&a,&b);
+        if (a == -1 && b == -1)
+        	break;
+        a = abs(a-b);
+       // printf("%d\n",a);
+        if (a>50) a=100-a;
+        printf("%d\n",a);
+    }
+    return 0;
 }

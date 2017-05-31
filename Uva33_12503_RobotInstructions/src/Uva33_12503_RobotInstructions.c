b6165e8c37450b6a9446af678130fb3b3d788822
Abdullah Khan <cmabdullah21@gmail.com>
2017-05-31 10:22:56 +0600

    RobotInstructions started

---
+++ Uva33_12503_RobotInstructions/src/Uva33_12503_RobotInstructions.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Uva33_12503_RobotInstructions.c
+ Author      : AbdullahKhan
+ Version     :
+ Copyright   : OpenSource
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("12503_RobotInstructions"); /* prints 12503_RobotInstructions */
+	return EXIT_SUCCESS;
+}

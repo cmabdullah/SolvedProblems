44817a15e2f4b61ef62f57840708dd8e9b3dac4f
Abdullah Khan <cmabdullah21@gmail.com>
2017-06-03 23:35:56 +0600

    Done 12503_RobotInstructions , awating for acception

--- Uva33_12503_RobotInstructions/src/Uva33_12503_RobotInstructions.c
+++ Uva33_12503_RobotInstructions/src/Uva33_12503_RobotInstructions.c
@@ -12,6 +12,38 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("12503_RobotInstructions"); /* prints 12503_RobotInstructions */
-	return EXIT_SUCCESS;
+	char str[100], sa[100];
+	int ptr, flag, n, i, v, T;
+
+	scanf("%d", &T);
+
+	while(T--){
+
+	scanf("%d", &n);
+	ptr = 0;
+	for (i = 0; i<n; i++){
+		scanf("%s",str);
+		if(strcmp(str,"LEFT")==0){
+			ptr--;
+			flag = 0;
+		}
+		else if(strcmp(str,"RIGHT")==0){
+			ptr++;
+			flag = 1;
+		}
+		else{
+			scanf("%s %d",str, &v);
+
+			if (flag == 0){
+				sa[i] = v;
+				ptr--;
+			}
+			else{
+				sa[i] = v;
+				ptr++;
+			}
+		}
+	}
+	printf("%d", ptr);
+	}
 }

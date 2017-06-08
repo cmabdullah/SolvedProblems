7936a383d0126602af78bb7aa80e6f3262752ff8
Abdullah Khan <cmabdullah21@gmail.com>
2017-06-08 19:38:29 +0600

    start Greedy Gift Givers

---
+++ Uva35_00119GreedyGiftGivers/src/Uva35_00119GreedyGiftGivers.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Uva35_00119GreedyGiftGivers.c
+ Author      : AbdullahKhan
+ Version     :
+ Copyright   : OpenSource
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("00119GreedyGiftGivers"); /* prints 00119GreedyGiftGivers */
+	return EXIT_SUCCESS;
+}

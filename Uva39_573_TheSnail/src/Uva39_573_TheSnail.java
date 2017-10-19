5589a3f2e4fc2995e6c95a3c940ab3a58c1b6cdf
cmabdullah <cmabdullah21@gmail.com>
2017-10-19 23:57:07 +0600

    Almost done 573

--- Uva39_573_TheSnail/src/Uva39_573_TheSnail.java
+++ Uva39_573_TheSnail/src/Uva39_573_TheSnail.java
@@ -1,52 +1,40 @@
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Uva39_573_TheSnail {
-/****
- * 
- Day Initial		 Height Distance 	Climbed Height After 	Climbing Height		 After Sliding
-		1 				0’					 3’ 						3’					 2’
-		2 				2’ 					2.7’ 					4.7’					 3.7’
-		3 				3.7’ 				2.4’						 6.1’				 −−
- * */
 
 	public static void main(String[] args) {
         Scanner obj = new Scanner(System.in);
-        StringBuffer s = new StringBuffer();
-        while(true){
-        	double H,U,D,F;
-        	double d= 0;
-        long days = 0;
-            H = obj.nextInt();
-            U = obj.nextInt(); 
-            	D = obj.nextInt();
-            	F = obj.nextInt();
-            if(H==0&&U==H&&D==0&&F==0){
-            	break;
-            	}
-            F = (U*F)/100.0;
-            while(true){
-                days++;
+        PrintWriter s = new PrintWriter(System.out);
+        while(obj.hasNext()){
+        	double H,U,D,F,decrease, current = 0;
+        	boolean first = true;
+        int days = 0;
+        H = obj.nextInt();
+        U = obj.nextInt();
+        D = obj.nextInt();
+        F = obj.nextInt();
+        if(H==0)
+        	break;
+        decrease = U*((U*F)/100.0) ;
 
-                if(U>=0){
-                d+= U;
-                }
-                if(d>H){
-                	break;
-                	}
-                d-= D;
-                if(d<0){
-                	break;
-                	}
-                U-= F;
-            }
-	        if(d>=0){
-	        	s.append("success on day "+days).append('\n');
-	        	}
-	        else{
-	        	s.append("failure on day " + days).append('\n');
-	        	}
+	        while(current <= H && (first || current >= 0)){
+	        		days++;
+	        		current = current+U;
+	           	if(current > H)break;
+	           	current = current=D;
+	           	U = U-decrease;
+	           	U = Math.max(U, 0);
+	           	first = false;
+	        }
+        if (current <= 0)
+            s.println("failure on day " + days);
+        else
+            s.println("success on day " + days);
+        s.flush();
         }
-        System.out.println(s.toString());
-
+        s.close();
+        obj.close();
+        
 	}
 }

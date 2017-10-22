136d7832e54d535271afc9989d8fa248aeaba16f
cmabdullah <cmabdullah21@gmail.com>
2017-10-22 23:56:53 +0600

    New problem start 661 Blowing Fuses

---
+++ UVA_40_661_Blowing_Fuses/src/UVA_40_661_Blowing_Fuses.java
@@ -0,0 +1,10 @@
+
+public class UVA_40_661_Blowing_Fuses {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("UVA_40_661_Blowing_Fuses");
+
+	}
+
+}
--- Uva39_573_TheSnail/src/Uva39_573_TheSnail.java
+++ Uva39_573_TheSnail/src/Uva39_573_TheSnail.java
@@ -4,36 +4,33 @@ import java.util.Scanner;
 public class Uva39_573_TheSnail {
 
 	public static void main(String[] args) {
-        Scanner obj = new Scanner(System.in);
-        PrintWriter s = new PrintWriter(System.out);
-        while(obj.hasNext()){
-        	double H,U,D,F,decrease, current = 0;
-        	boolean first = true;
-        int days = 0;
-        H = obj.nextInt();
-        U = obj.nextInt();
-        D = obj.nextInt();
-        F = obj.nextInt();
-        if(H==0)
-        	break;
-        decrease = U*(F*1.0/100.0) ;
-
-	        while(current <= H && (first || current >= 0)){
-	        		days++;
-	        		current = current+U;
-	           	if(current > H)break;
-	           	current = current-D;
-	           	U = U-decrease;
-	           	U = Math.max(U, 0);
-	           	first = false;
-	        }
-        if (current <= 0)
-            s.println("failure on day " + days);
-        else
-            s.println("success on day " + days);
-       s.flush();
-        }
-        //s.close();
-       // obj.close();
+		Scanner obj = new Scanner(System.in);
+		PrintWriter s = new PrintWriter(System.out);
+		while(obj.hasNext()){
+			double H,U,D,F,decrease, current = 0;
+			boolean first = true;
+			int days = 0;
+			H = obj.nextInt();
+			U = obj.nextInt();
+			D = obj.nextInt();
+			F = obj.nextInt();
+			if(H==0)
+				break;
+			decrease = U*(F*1.0/100.0) ;
+			while(current <= H && (first || current >= 0)){
+				days++;
+				current = current+U;
+				if(current > H)break;
+				current = current-D;
+				U = U-decrease;
+				U = Math.max(U, 0);
+				first = false;
+			}
+			if (current <= 0)
+				s.println("failure on day " + days);
+			else
+				s.println("success on day " + days);
+			s.flush();
+		}
 	}
 }

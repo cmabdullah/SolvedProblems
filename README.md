0c4860251573c364a82f524d248388a28a92c910
cmabdullah <cmabdullah21@gmail.com>
2017-11-04 15:17:25 +0600

    10107 runtime error

---
+++ README.md
@@ -0,0 +1,22 @@
+
+# EOF
+
+```java
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class UVa43_10107_WhatIsTheMedian {
+	// Competitive programming 3 page 42
+	//type LINEAR DS WITH BUILT-IN LIBRARIES
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String s;
+		while ((s=br.readLine())!=null) {
+			System.out.println(s);
+		}
+
+	}
+}
+```
--- UVa43_10107_WhatIsTheMedian?/src/UVa43_10107_WhatIsTheMedian.java
+++ UVa43_10107_WhatIsTheMedian?/src/UVa43_10107_WhatIsTheMedian.java
@@ -1,9 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class UVa43_10107_WhatIsTheMedian {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	// Competitive programming 3 page 42
+	//type LINEAR DS WITH BUILT-IN LIBRARIES
+	//https://www.mathsisfun.com/definitions/median.html
+	public static void main(String[] args) throws IOException {
+		long[] val = new long[1000];
+		InputStreamReader r=new InputStreamReader(System.in);  
+		BufferedReader br=new BufferedReader(r);  
+		String s;
+		int i = 0;
+		Arrays.fill(val, Long.MAX_VALUE);
+		while ((s=br.readLine())!= null) {
+			
+			StringTokenizer x = new StringTokenizer(s);
+			val[i++] = Long.parseLong(x.nextToken());
+			Arrays.sort(val);
+			if (i==1) {
+				System.out.println(val[0]);
+			} else if (i%2==0) {
+				System.out.println((val[i/2-1]+val[i/2])/2);
+			} else {
+				System.out.println(val[i/2]);
+			}
+		}
 	}
-
 }

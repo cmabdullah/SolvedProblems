b34864d71aa7f38f0bcfe51774dbe0dafd66bd96
cmabdullah <cmabdullah21@gmail.com>
2017-11-05 22:45:10 +0600

    101 - The Blocks Problem.pdf start

---
+++ UVa44_101_TheBlocksProblem/src/UVa44101_TheBlocksProblem.java
@@ -0,0 +1,11 @@
+
+public class UVa44101_TheBlocksProblem {
+
+	public static void main(String[] args) {
+		// Competitive programming 3 page 41
+		//2D Array Manipulation
+
+		System.out.println("Hello world");
+	}
+
+}

f461f29cc4856f2e2d4146a9535bfde876e222b6
cmabdullah <cmabdullah21@gmail.com>
2017-11-24 12:19:04 +0600

    Done PDA

--- PDA/src/PDA.cpp
+++ PDA/src/PDA.cpp
@@ -8,8 +8,72 @@
 
 #include <iostream>
 using namespace std;
-
-int main() {
-	cout << "cmmcmcmmc" << endl; // prints cmmcmcmmc
-	return 0;
+#include<stdio.h>
+#define EOS '\0'
+int TOS= -1;
+char stack[100];
+char input[100];
+char inp;
+int state = 1 , i=0;;
+int pop(void);
+void push(char x);
+int main(){
+    scanf("%s",input);
+    while(1){
+    	inp=input[i];
+        if(state==1) {
+        	state=2;
+            push('$');
+        }
+        if(state==2) {
+			if(inp=='a' || inp=='b'){
+				if(inp=='a') {
+					state=2;
+					push(inp);
+				}
+				else if(inp=='b') {
+					state=2;
+					push(inp);
+				}
+			}
+			else if(inp=='#')
+				state=3;
+				else {
+					state=5;
+				}
+        }
+        else  if(state==3) {
+            if(inp=='a' && stack[TOS]=='a') {
+            		state=3;
+                pop();
+            }
+           else if(inp=='b' && stack[TOS]=='b') {
+        	   	   state=3;
+                pop();
+            }
+           else if(inp==EOS && stack[TOS]=='$'){
+        	   	   state=4;
+            }
+           else{
+        	   	   state=5;
+            }
+        }
+        else  if(state==4){
+        	printf("Accepted\n");
+        	break;
+        }
+        else{
+        		state=5;
+        		printf("Rejected\n");
+            break;
+        }
+        i++;
+    }// whle loop
+    return  0;
+}
+void push(char x) {
+	stack[++TOS]= x;
+}
+int pop() {
+    return  stack[TOS--];
 }
--- TOCAssignment2/src/TOCAssignment2.c
+++ TOCAssignment2/src/TOCAssignment2.c
@@ -7,7 +7,7 @@
  Description : Hello World in C, Ansi-style
  ============================================================================
  */
-
+/****
 #include<stdio.h>
 char input[100] ;
 char stack[100] ;
@@ -86,4 +86,76 @@ void state4(){
 }
 void reject(){
     printf("Reject\n");
+}
+
+***/
+
+#include<stdio.h>
+#define EOS '\0'
+int TOS= -1;
+char stack[100];
+char input[100];
+char inp;
+int state = 1 , flag=0;;
+int pop(void);
+void push(char x);
+int main(){
+    scanf("%s",input);
+    while(1){
+    	inp=input[flag];
+        if(state==1) {
+        	state=2;
+            push('$');
+        }
+        if(state==2) {
+			if(inp=='a' || inp=='b'){
+				if(inp=='a') {
+					state=2;
+					push(inp);
+				}
+				else if(inp=='b') {
+					state=2;
+					push(inp);
+				}
+			}
+			else if(inp=='#')
+				state=3;
+				else {
+					state=5;
+				}
+        }
+        else  if(state==3) {
+            if(inp=='a' && stack[TOS]=='a') {
+            		state=3;
+                pop();
+            }
+           else if(inp=='b' && stack[TOS]=='b') {
+        	   	   state=3;
+                pop();
+            }
+           else if(inp==EOS && stack[TOS]=='$'){
+        	   	   state=4;
+            }
+           else{
+        	   	   state=5;
+            }
+        }
+        else  if(state==4){
+        	printf("Accepted\n");
+        	break;
+        }
+        else{
+        		state=5;
+        		printf("Rejected\n");
+            break;
+        }
+        flag++;
+    }// whle loop
+    return  0;
+}
+void push(char x) {
+	stack[++TOS]= x;
+}
+int pop() {
+    return  stack[TOS--];
 }

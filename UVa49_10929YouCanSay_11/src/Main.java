fab8058e0c26d0952c2179e5c05e315d5a4fa4f9
cmabdullah <cmabdullah21@gmail.com>
2018-04-22 11:32:15 +0600

    runtime error

---
+++ UVa49_10929YouCanSay_11/src/Main.java
@@ -0,0 +1,23 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		String input;
+		final BigInteger eleven = new BigInteger("11");
+		while (s.hasNextLine()){
+			input = s.nextLine();
+			BigInteger X = new BigInteger(input);
+			if (X.equals(BigInteger.ZERO))
+				break;
+				
+				
+			if (X.mod(eleven).equals(BigInteger.ZERO))
+				System.out.println(input +" is a multiple of 11." );
+			else
+				System.out.println(input +" is not a multiple of 11.");
+			input = s.next();
+		}
+	}
+}
